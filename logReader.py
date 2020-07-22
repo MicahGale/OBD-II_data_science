85598a9ad259f721ab9bfdb47e0d2d644cd3dd7a
Micah Gale <mgale@fastmail.com>
2020-07-22 05:47:30 -0600

    Made script for importing new logs
    -Still needs to track which old logs succeeded.

---
+++ logReader.py
@@ -0,0 +1,38 @@
+from carPointer import *
+import glob
+import logParser
+import os
+import psycopg2 as psql
+
+
+
+def findNewLogs(car, dbConn):
+    logBase = LOG_BASES[car.VIN]
+    loggingFile = os.path.join(logBase, "Parsed.txt")
+    if not os.path.exists(loggingFile):
+        with open(loggingFile, 'w'):
+            os.utime(loggingFile)
+    with open(os.path.join(logBase, "Parsed.txt"),"r") as fh:
+        filesParsed = {}
+        for row in fh:
+            filesParsed[row] = 1
+        filesToRead = []
+        for fileName in glob.glob(os.path.join(logBase,"*/*")):
+            if fileName not in filesParsed:
+                filesToRead.append(fileName)
+        tripLegs = logParser.parseFilesBatch(filesToRead)
+        car.getTripsFromGoogleDrive(dbConn)
+        car.addTripLegs(logs)
+        car.matchUpLogsAndData()
+        car.writeToDB(dbConn)
+        #TODO track success for these files,
+        #TODO write the new ones out to the file
+
+
+try:
+    dbConn = psql.connect("dbname=carData user=mgale")
+    for car in cars:
+        findNewLogs(car, dbConn)
+finally:
+    dbConn.close()
+
